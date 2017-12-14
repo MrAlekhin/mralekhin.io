852e93227d275f44f6ffe2ca724c59b7d9e137be
artem1998 <arty-haro@yandex.ru>
2017-12-13 19:42:13 -0500

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 
+//add the ne todo
 app.post('/todos', (req, res)=>{
   var todo = new Todo({
     text: req.body.text
@@ -25,6 +26,7 @@ app.post('/todos', (req, res)=>{
   });
 });
 
+//gets the list of todos
 app.get('/todos', (req, res)=>{
   Todo.find().then((todos)=>{
     res.send({todos});
@@ -33,6 +35,7 @@ app.get('/todos', (req, res)=>{
   });
 })
 
+//gets the specific todo by id
 app.get('/todos/:id', (req, res)=>{
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
@@ -48,6 +51,7 @@ app.get('/todos/:id', (req, res)=>{
   });
 });
 
+//delets the todo by id
 app.delete('/todos/:id', (req, res)=>{
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
@@ -62,6 +66,8 @@ app.delete('/todos/:id', (req, res)=>{
     res.status(400).send();
   });
 });
+
+//editing the todo by id
 app.patch('/todos/:id', (req, res)=>{
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
@@ -87,6 +93,20 @@ app.patch('/todos/:id', (req, res)=>{
     res.status(400).send();
   })
 })
+
+//adds the new user
+app.post('/users', (req, res)=>{
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user.save().then((user)=>{
+    return user.generateAuthToken();
+  }).then((token)=>{
+    res.header('x-auth', token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, ()=>{
   console.log(`Started up at port ${port}`);
 });
