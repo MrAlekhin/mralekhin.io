2be06c9f36f4e21851a0857889db22985709f94d
Artem1998 <arty-hero@yandex.ru>
2017-12-19 01:12:10 -0500

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -108,9 +108,22 @@ app.post('/users', (req, res)=>{
   });
 });
 
+//authenticate the user
 app.get('/users/me', authenticate, (req, res)=>{
   res.send(req.user);
 })
+
+app.post('/users/login', (req, res)=>{
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user)=>{
+    user.generateAuthToken().then((token)=>{
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) =>{
+    res.status(400).send();
+  });
+});
 
 app.listen(port, ()=>{
   console.log(`Started up at port ${port}`);
